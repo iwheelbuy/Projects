612de29a9041fd8b449d7dcaf70b8aa96286c871
iWheelBuy <iwheelbuy@protonmail.com>
2020-07-09 02:01:23 +0700

    collectByTime

--- Tests/Combine/Publishers/Reducing Elements/CollectByTime.swift
+++ Tests/Combine/Publishers/Reducing Elements/CollectByTime.swift
@@ -5,28 +5,29 @@ import DependenciesTest
 final class CollectByTime: TestCase {
 
    func test_common_behavior() {
-      let configuration = TestScheduler.Configuration.default
-      let scheduler = TestScheduler()
-      let upstream: TestablePublisher<String, TestError> = scheduler.createRelativeTestablePublisher([
-         (1, .input("a")),
-         (2, .input("b")),
-         (3, .input("c")),
-         (4, .input("d")),
-         (5, .input("e")),
-         (6, .input("f")),
-         (7, .input("g")),
-         (8, .input("h")),
-         (9, .input("i")),
-         (10, .completion(.finished))
+      let upstream: TestablePublisher<Int, TestError> = scheduler.createRelativeTestablePublisher([
+         (25, .input(1)),
+         (26, .input(2)),
+         (27, .input(3)),
+         (28, .input(4)),
+         (28, .input(5)),
+         (29, .input(6)),
+         (30, .input(7)),
+         (99, .input(8)),
+         (99, .completion(.finished))
       ])
-      let window: VirtualTimeInterval = 3
+      let count: Int = 5
+      let interval: VirtualTimeInterval = 10
       let strategy = Publishers.TimeGroupingStrategy<TestScheduler>
-         .byTime(scheduler, window)
+         .byTimeOrCount(scheduler, interval, count)
       let publisher = Publishers.CollectByTime(
          upstream: upstream,
          strategy: strategy,
          options: nil
       )
       let subscriber = scheduler.start(configuration: configuration, create: { publisher })
+      XCTAssertEqual(subscriber.inputs, [[1, 2, 3, 4, 5], [6, 7], [8]])
+      XCTAssertTrue(subscriber.subscribed(time: configuration.subscribed))
+      XCTAssertTrue(subscriber.completed(time: configuration.subscribed + 99))
    }
 }
