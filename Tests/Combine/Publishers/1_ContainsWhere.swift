ad9efea0acd9ddda9b27f1f5a967158c5cdedb89
iWheelBuy <iwheelbuy@protonmail.com>
2020-07-13 10:25:44 +0700

    +

---
+++ Tests/Combine/Publishers/1_ContainsWhere.swift
@@ -0,0 +1,53 @@
+import DependenciesTest
+/*
+ Если upstream публикует ожидаемое значение, то downstream публикует true и
+ успешно завершается. Если downstream так и не дожидается ожидаемого значения,
+ то непосредственно перед успешным завершением будет опубликовано false. Если
+ upstream не публикует ожидаемого значения и заканчивается ошибкой, то
+ downstream не опубликут ничего и завершится ошибкой.
+ */
+final class ContainsWhere: XCTestCase {
+
+   func test_common_behavior() {
+      for index in [0, 1] {
+         let storage = TestStorage()
+         let events: [TestEvent<String>] = [
+            .value("a", at: 0),
+            .value("b", at: 1),
+            .success(at: 2)
+         ]
+         let upstream = storage.publisher(events: events)
+         if index == 0 {
+            let publisher = Publishers.ContainsWhere(upstream: upstream, predicate: { $0 == "b" })
+            let completion = publisher.success(at: 1)
+            storage.test(publisher, completion: completion) { results in
+               XCTAssertEqual(results.values, [true])
+               XCTAssertEqual(results.times, [1])
+            }
+         } else {
+            let publisher = Publishers.ContainsWhere(upstream: upstream, predicate: { $0 == "c" })
+            let completion = publisher.success(at: 2)
+            storage.test(publisher, completion: completion) { results in
+               XCTAssertEqual(results.values, [false])
+               XCTAssertEqual(results.times, [2])
+            }
+         }
+      }
+   }
+
+   func test_failure_behavior() {
+      let storage = TestStorage()
+      let events: [TestEvent<String>] = [
+         .value("a", at: 0),
+         .value("b", at: 1),
+         .failure(.default, at: 2)
+      ]
+      let upstream = storage.publisher(events: events)
+      let publisher = Publishers.ContainsWhere(upstream: upstream, predicate: { $0 == "c" })
+      let completion = publisher.failure(.default, at: 2)
+      storage.test(publisher, completion: completion) { results in
+         XCTAssertEqual(results.values, [])
+         XCTAssertEqual(results.times, [])
+      }
+   }
+}
---
+++ Tests/Combine/Publishers/1_TryAllSatisfy.swift
@@ -0,0 +1,67 @@
+import DependenciesTest
+/*
+ Если все значения из upstream подходят под условие, то после завершения
+ upstream будет опубликовано true и успешное завершение. В противном случае
+ будет опубликовано false и успешное завершение, но только после успешного
+ завершения upstream.
+ */
+final class TryAllSatisfy: XCTestCase {
+
+   func test_common_behavior() {
+      let storage = TestStorage()
+      let values = Array(0 ... 100)
+         .map({ _ in Int(arc4random_uniform(50)) })
+         .set
+         .sorted()
+      let max = values.max()!
+      let events: [TestEvent<Int>] = values
+         .map({ TestEvent(case: .value($0), time: $0) })
+         .appending({ _ in TestEvent<Int>.success(at: max) })
+      let upstream = storage.publisher(events: events)
+      let publisher = Publishers.TryAllSatisfy(upstream: upstream, predicate: { $0 <= max })
+      let completion = publisher.success(at: max)
+      storage.test(publisher, completion: completion) { results in
+         XCTAssertEqual(results.values, [true])
+         XCTAssertEqual(results.times, [max])
+      }
+   }
+
+   func test_failure_behavior() {
+      let storage = TestStorage()
+      let values = Array(0 ... 100)
+         .map({ _ in Int(arc4random_uniform(50)) })
+         .set
+         .sorted()
+      let max = values.max()!
+      let events: [TestEvent<Int>] = values
+         .map({ TestEvent(case: .value($0), time: $0) })
+         .appending({ _ in TestEvent<Int>.failure(.default, at: max) })
+      let upstream = storage.publisher(events: events)
+      let publisher = Publishers.TryAllSatisfy(upstream: upstream, predicate: { $0 <= max })
+      let completion = publisher.failure(.default, at: max)
+      storage.test(publisher, completion: completion) { results in
+         XCTAssertEqual(results.values, [])
+         XCTAssertEqual(results.times, [])
+      }
+   }
+
+   func test_throwing_behavior() {
+      let storage = TestStorage()
+      let values = Array(0 ... 100)
+         .map({ _ in Int(arc4random_uniform(50)) })
+         .set
+         .sorted()
+      let max = values.max()!
+      let min = values.min()!
+      let events: [TestEvent<Int>] = values
+         .map({ TestEvent(case: .value($0), time: $0) })
+         .appending({ _ in TestEvent<Int>.failure(.default, at: max) })
+      let upstream = storage.publisher(events: events)
+      let publisher = Publishers.TryAllSatisfy(upstream: upstream, predicate: { _ in throw TestError.thrown })
+      let completion = publisher.failure(.thrown, at: min)
+      storage.test(publisher, completion: completion) { results in
+         XCTAssertEqual(results.values, [])
+         XCTAssertEqual(results.times, [])
+      }
+   }
+}
