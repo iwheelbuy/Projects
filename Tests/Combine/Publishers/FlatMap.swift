6f1ec89e1a0a8e6e35543242b388b3f453324e96
iWheelBuy <iwheelbuy@protonmail.com>
2020-07-18 11:03:16 +0700

    +

--- Tests/Combine/Publishers/FlatMap.swift
+++ Tests/Combine/Publishers/FlatMap.swift
@@ -1,45 +1,72 @@
-//import DependenciesTest
-///*
-// Если все значения из upstream подходят под условие, то после завершения
-// upstream будет опубликовано true и успешное завершение. В противном случае
-// будет опубликовано false и успешное завершение, но только после успешного
-// завершения upstream.
-// */
-//final class FlatMap: XCTestCase {
-//
-//   func test_common_behavior() {
-//      let handler = TestHandler()
-//      let values = Array(0 ... 100)
-//         .map({ _ in Int(arc4random_uniform(50)) })
-//         .set
-//         .sorted()
-//      let max = values.max()!
-//      let events: [TestEvent<Int>] = values
-//         .map({ TestEvent(case: .value($0), time: $0) })
-//         .appending({ _ in TestEvent<Int>.success(at: max) })
-//      let upstream = handler.publisher(events: events)
-//      let publisher = Publishers.FlatMap(upstream: <#T##_#>, maxPublishers: <#T##Subscribers.Demand#>, transform: <#T##(_.Output) -> _#>)
-//      let completion = publisher.success(at: max)
-//      let results = handler.test(publisher, completion: completion)
-//      XCTAssertEqual(results.values, [true])
-//      XCTAssertEqual(results.times, [max])
-//   }
-//
-//   func test_failure_behavior() {
-//      let handler = TestHandler()
-//      let values = Array(0 ... 100)
-//         .map({ _ in Int(arc4random_uniform(50)) })
-//         .set
-//         .sorted()
-//      let max = values.max()!
-//      let events: [TestEvent<Int>] = values
-//         .map({ TestEvent(case: .value($0), time: $0) })
-//         .appending({ _ in TestEvent<Int>.failure(.default, at: max) })
-//      let upstream = handler.publisher(events: events)
-//      let publisher = Publishers.AllSatisfy(upstream: upstream, predicate: { $0 <= max })
-//      let completion = publisher.failure(.default, at: max)
-//      let results = handler.test(publisher, completion: completion)
-//      XCTAssertEqual(results.values, [])
-//      XCTAssertEqual(results.times, [])
-//   }
-//}
+import DependenciesTest
+/*
+ Для каждого значения из upstream пораждается новый поток данных. Все
+ порожденные потоки публикуют значения в downstream. Пораждение каждого нового
+ потока никак не влияет на ранее поражденные потоки. Заканчивается успехом
+ после того, как все поражденные потоки завершатся успехом. Заканчивается
+ ошибкой после того, как любой из поражденных потоков завершится ошибкой.
+ */
+final class FlatMap: XCTestCase {
+
+   func test_common_behavior() {
+
+      func makeUpstream(value: Int) -> AnyPublisher<Int, TestError> {
+         let events: [TestEvent<Int>] = [
+            .value(value, at: 0),
+            .value(value + 10, at: 3),
+            .success(at: 4)
+         ]
+         return handler.publisher(absolute: false, events: events)
+      }
+
+      let handler = TestHandler()
+      let events: [TestEvent<Int>] = [
+         .value(0, at: 0),
+         .value(1, at: 2),
+         .success(at: 3)
+      ]
+      let upstream = handler.publisher(events: events)
+      let publisher = Publishers.FlatMap(
+         upstream: upstream,
+         maxPublishers: .unlimited,
+         transform: { value -> AnyPublisher<Int, TestError> in
+            return makeUpstream(value: value)
+         }
+      )
+      let completion = publisher.success(at: 6)
+      let results = handler.test(publisher, completion: completion)
+      XCTAssertEqual(results.values, [0, 1, 10, 11])
+      XCTAssertEqual(results.times, [0, 2, 3, 5])
+   }
+
+   func test_failure_behavior() {
+
+      func makeUpstream(value: Int) -> AnyPublisher<Int, TestError> {
+         let events: [TestEvent<Int>] = [
+            .value(value, at: 0),
+            .value(value + 10, at: 3),
+            .failure(.custom("\(value)"), at: 4)
+         ]
+         return handler.publisher(absolute: false, events: events)
+      }
+
+      let handler = TestHandler()
+      let events: [TestEvent<Int>] = [
+         .value(0, at: 0),
+         .value(1, at: 2),
+         .success(at: 3)
+      ]
+      let upstream = handler.publisher(events: events)
+      let publisher = Publishers.FlatMap(
+         upstream: upstream,
+         maxPublishers: .unlimited,
+         transform: { value -> AnyPublisher<Int, TestError> in
+            return makeUpstream(value: value)
+         }
+      )
+      let completion = publisher.failure(.custom("0"), at: 4)
+      let results = handler.test(publisher, completion: completion)
+      XCTAssertEqual(results.values, [0, 1, 10])
+      XCTAssertEqual(results.times, [0, 2, 3])
+   }
+}
